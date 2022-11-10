fee64b954a4cdd0fb8513e697aa0c4abcc2dddc7
pospij <110195254+pospij@users.noreply.github.com>
2022-11-10 20:16:16 +0800

    Correction
    correction 1. question

--- Question_Script.sql
+++ Question_Script.sql
@@ -1,38 +1,41 @@
-/* Otazka 1 Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?*/
+/*1. otazka
+ *   Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+ */
+
 CREATE OR REPLACE VIEW v_up_down AS
 	WITH add_next_row AS (
-		SELECT 
-			cpib.name,
-			cp.payroll_year,
-			cp.value,
-			LEAD(cp.value) OVER (ORDER BY cp.industry_branch_code, cp.payroll_year) AS next_row
-		FROM czechia_payroll cp 
-		JOIN czechia_payroll_industry_branch cpib 
-		ON cp.industry_branch_code = cpib.code 
-		AND cp.value_type_code = 5958
+		SELECT
+			`year` ,
+			salary ,
+			name_of_industry, 
+			LEAD(salary) OVER (ORDER BY name_of_industry, `year`  ) AS next_row
+		FROM t_Jan_Pospisil_project_SQL_primary_final
 		)
-	SELECT payroll_year ,
-			name,
+	SELECT `year`,
+			name_of_industry ,
 			CASE
-				WHEN value < next_row THEN 1
-				WHEN value = next_row THEN 0
+				WHEN salary  < next_row THEN 1
+				WHEN salary  = next_row THEN 0
 				ELSE -1
 			END AS 'up_down'
 	FROM add_next_row
 
 SELECT
-	name AS 'Industry_branch_name',
+	name_of_industry AS 'Industry_branch_name',
 	CASE 
 		WHEN SUM(up_down) > 0 THEN 'V průběhu let mzdy stoupají'
 		WHEN SUM(up_down) < 0 THEN 'V průběhu let mzdy klesají'
 		ELSE 'V průběhu let jsou mzdy stejné'
-	END AS 'result_between_2000/2021'
+	END AS 'result_between_2006/2018'
 FROM v_up_down 
-GROUP BY name;
+GROUP BY name_of_industry;
 
 
 
-SELECT * FROM t_Jan_Pospisil_project_SQL_primary_final 
+
+/* 2. otazka
+ * Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+ */ 
 
 SELECT
 	`year`,
