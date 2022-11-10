344713daf4f64cfdedd08977bb3f59aec906cba1
pospij <110195254+pospij@users.noreply.github.com>
2022-11-10 19:36:56 +0800

    Add2
    Add project table and one more question

--- Question_Script.sql
+++ Question_Script.sql
@@ -1,4 +1,4 @@
-///Otazka 1 Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?///
+/* Otazka 1 Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?*/
 CREATE OR REPLACE VIEW v_up_down AS
 	WITH add_next_row AS (
 		SELECT 
@@ -29,3 +29,28 @@ SELECT
 	END AS 'result_between_2000/2021'
 FROM v_up_down 
 GROUP BY name;
+
+
+
+SELECT * FROM t_Jan_Pospisil_project_SQL_primary_final 
+
+SELECT
+	`year`,
+	ROUND(AVG(salary)),
+	ROUND(AVG(salary) / value)
+FROM t_Jan_Pospisil_project_SQL_primary_final
+WHERE product_code IN ('111301','114201')
+AND `year` IN ('2006','2018')
+GROUP BY `year`,product_code 
+
+SELECT
+	`year`,
+	ROUND(AVG(salary)) AS 'AVG salary',
+	CASE 
+		WHEN product_code = 111301 THEN CONCAT(ROUND(AVG(salary)/ value), ' kg chleba za prumerny plat')
+		WHEN product_code = 114201 THEN CONCAT(ROUND(AVG(salary)/ value), ' l mleka za prumerny plat')
+	END AS 'result'
+FROM t_Jan_Pospisil_project_SQL_primary_final
+WHERE product_code IN ('111301','114201')
+AND `year` IN ('2006','2018')
+GROUP BY `year`,product_code 
---
+++ project_table.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE VIEW v_tab_1 AS (
+SELECT cp.payroll_year,
+	ind.name ,
+	cp.value
+	FROM czechia_payroll AS cp
+JOIN czechia_payroll_industry_branch AS ind
+	ON cp.industry_branch_code = ind.code
+WHERE cp.value IS NOT NULL AND cp.value_type_code = 5958
+GROUP BY ind.name ,cp.payroll_year)
+;
+CREATE OR REPLACE VIEW v_tab_2 AS (
+SELECT 
+	cp.category_code,
+	cpc.name,
+	cpc.price_value,
+	cpc.price_unit,
+	cp.value,
+	YEAR(cp.date_from) 
+FROM czechia_price cp
+JOIN czechia_price_category cpc 
+ON cp.category_code  = cpc.code
+GROUP BY cp.category_code,YEAR(cp.date_from))
+;
+CREATE OR REPLACE VIEW v_cze_GDP AS 
+SELECT GDP ,
+	`year` 
+FROM economies
+WHERE country LIKE '%Czech%' AND GDP IS NOT NULL;
+SELECT * FROM v_cze_gdp ;
+
+
+CREATE TABLE IF NOT EXISTS t_Jan_Pospisil_project_SQL_primary_final AS
+SELECT
+	v_tab_1.payroll_year AS 'year',
+	v_tab_1.name  AS 'name_of_industry',
+	v_tab_1.value  AS 'salary',
+	v_tab_2.name AS 'product_name',
+	CONCAT(v_tab_2.price_value, v_tab_2.price_unit) AS 'unit',
+	v_tab_2.value,
+	v_tab_2.category_code AS 'product_code',
+	v_cze_gdp.GDP 
+FROM v_tab_1
+LEFT JOIN v_tab_2 
+ON v_tab_1.payroll_year = v_tab_2.`YEAR(cp.date_from)` 
+JOIN v_cze_gdp
+ON payroll_year = v_cze_gdp.year
+WHERE payroll_year BETWEEN 2006 AND 2018
+ORDER BY payroll_year ;
+
+#DROP TABLE t_Jan_Pospisil_project_SQL_primary_final ;
+
+#SELECT *
+#FROM t_Jan_Pospisil_project_SQL_primary_final ;
