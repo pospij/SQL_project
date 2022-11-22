b00eba98c4705550bacff1713f30caa111bd3ef0
pospij <110195254+pospij@users.noreply.github.com>
2022-11-22 16:15:55 +0800

    Update Question_Script.sql
    - new query for 1st question
    - query for 2nd question, update for better readability
    - update query for 4th question, add salary growth
    - change view names for better overview
    - Trim spaces
    -Modification an addition of the semicolon

--- Question_Script.sql
+++ Question_Script.sql
@@ -3,34 +3,13 @@
  *  Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
  */
 
-CREATE OR REPLACE VIEW v_up_down AS
-	WITH add_next_row AS (
-		SELECT
-			`year` ,
-			salary ,
-			name_of_industry, 
-			LEAD(salary) OVER (ORDER BY name_of_industry, `year`  ) AS next_row
-		FROM t_Jan_Pospisil_project_SQL_primary_final
-		)
-	SELECT `year`,
-			name_of_industry ,
-			CASE
-				WHEN salary  < next_row THEN 1
-				WHEN salary  = next_row THEN 0
-				ELSE -1
-			END AS 'up_down'
-	FROM add_next_row
-
-SELECT
-	name_of_industry AS 'Industry_branch_name',
-	CASE 
-		WHEN SUM(up_down) > 0 THEN 'V průběhu let mzdy stoupají'
-		WHEN SUM(up_down) < 0 THEN 'V průběhu let mzdy klesají'
-		ELSE 'V průběhu let jsou mzdy stejné'
-	END AS 'result_between_2006/2018'
-FROM v_up_down 
-GROUP BY name_of_industry
-;
+SELECT 
+	`year`,
+	name_of_industry,
+	salary,
+	ROUND((((LEAD(salary) OVER (PARTITION BY name_of_industry ORDER BY name_of_industry, year)) - salary) / salary) * 100, 2) AS 'perecent_salary_growth'
+FROM t_Jan_Pospisil_project_SQL_primary_final 
+GROUP BY  name_of_industry,`year`;
 
 
 
@@ -40,27 +19,21 @@ GROUP BY name_of_industry
  *  Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
  */ 
 
-SELECT
-	`year`,
-	ROUND(AVG(salary)),
-	ROUND(AVG(salary) / value)
-FROM t_Jan_Pospisil_project_SQL_primary_final
-WHERE product_code IN ('111301','114201')
-AND `year` IN ('2006','2018')
-GROUP BY `year`,product_code 
 
 SELECT
 	`year`,
 	ROUND(AVG(salary)) AS 'avg_salary',
 	CASE 
-		WHEN product_code = 111301 THEN CONCAT(ROUND(AVG(salary)/ value), ' kg chleba za prumerny plat')
-		WHEN product_code = 114201 THEN CONCAT(ROUND(AVG(salary)/ value), ' l mleka za prumerny plat')
+		WHEN cpc.name LIKE '%Chléb%' THEN CONCAT(ROUND(AVG(salary)/ value), ' kg chleba za prumerny plat')
+		WHEN cpc.name LIKE '%Mléko%' THEN CONCAT(ROUND(AVG(salary)/ value), ' l mleka za prumerny plat')
 	END AS 'result'
-FROM t_Jan_Pospisil_project_SQL_primary_final
-WHERE product_code IN ('111301','114201')
+FROM t_Jan_Pospisil_project_SQL_primary_final prim
+JOIN czechia_price_category cpc
+ON prim.product_code = cpc.code 
+WHERE product_code IN('111301','114201')
 AND `year` IN ('2006','2018')
-GROUP BY `year`,product_code 
-;
+GROUP BY `year`,product_code;
+
 
 /*
  * 3. otazka 
@@ -68,20 +41,20 @@ GROUP BY `year`,product_code
  */
 WITH help_tab AS ( 
 	SELECT 
-		`year` ,
-		product_name ,
-		value ,
-		LEAD(value) OVER (ORDER BY product_name, `year`) AS 'value2'
+		`year`,
+		product_name,
+		value,
+		LEAD(value) OVER(ORDER BY product_name, `year`) AS 'value2'
 FROM t_Jan_Pospisil_project_SQL_primary_final
 GROUP BY product_name, `year` )
 SELECT
-	product_name ,
-	SUM(ROUND(((value2 - value) / value ) *100 , 2)) AS 'sum_growth_percent' 
+	product_name,
+	SUM(ROUND(((value2 - value) / value ) *100, 2)) AS 'sum_growth_percent' 
 FROM help_tab
 WHERE year != 2018
-GROUP BY product_name 
-ORDER BY sum_growth_percent ASC
-;
+GROUP BY product_name  
+ORDER BY sum_growth_percent ASC;
+
 
 /*
  * 4. otazka 
@@ -89,23 +62,37 @@ ORDER BY sum_growth_percent ASC
  */
 CREATE OR REPLACE VIEW v_food_growth AS 
 WITH help_tab AS ( 
-	SELECT 
-		`year` ,
-		product_name ,
-		value ,
+	SELECT
+		`year`,
+		product_name,
+		value,
 		LEAD(value) OVER (ORDER BY product_name, `year`) AS 'value2'
 FROM t_Jan_Pospisil_project_SQL_primary_final
 GROUP BY product_name, `year` )
 SELECT
-	(`year` + 1) AS 'year' ,
+	(`year` + 1) AS 'year',
 	ROUND(AVG(ROUND(((value2 - value) / value ) *100 , 2)),2) AS 'food_growth_percent' 
 FROM help_tab
 WHERE`year` != 2018
-GROUP BY (`year` + 1)
-;
-SELECT *
-FROM v_food_growth 
-;
+GROUP BY (`year` + 1);
+
+CREATE OR REPLACE VIEW v_salary_growth AS
+SELECT 
+	`year`,
+	ROUND(((LEAD(AVG(salary)) OVER (ORDER BY `year`) - AVG(salary)) / AVG(salary)) * 100,2)AS 'growth'
+FROM t_Jan_Pospisil_project_SQL_primary_final
+GROUP BY `year`;
+
+SELECT
+	fg.`year`,
+	fg.food_growth_percent,
+	sg.growth AS 'salary_growth_percent',
+	ABS(ABS(fg.food_growth_percent)- sg.growth) AS 'difference'
+FROM v_food_growth fg
+LEFT JOIN v_salary_growth sg
+ON fg.`year` = sg.`year`;
+
+
 /*
  * 5. otazka
  * Má výška HDP vliv na změny ve mzdách a cenách potravin?
@@ -116,19 +103,18 @@ FROM v_food_growth
 WITH growth_tab AS (
 	SELECT 
 		`year`,
-		GDP ,
+		GDP,
 		LEAD(GDP) OVER (ORDER BY `year`) AS 'GDP_nxt_row' 
 	FROM t_Jan_Pospisil_project_SQL_primary_final
 	GROUP BY `year`)
 SELECT 
 	(gt.`year`+ 1) AS 'year',
-	AVG(prim.salary) AS 'avg_salary',
-	vfg.growth_percent , 
+	ROUND(AVG(prim.salary),2) AS 'avg_salary',
+	vfg.food_growth_percent, 
 	ROUND(((GDP_nxt_row - gt.GDP) / gt.GDP) * 100 , 2) AS 'GDP_growth'
 FROM growth_tab gt
 JOIN v_food_growth vfg 
 ON (gt.`year`+ 1) = vfg.`year`
 JOIN t_Jan_Pospisil_project_SQL_primary_final prim
 ON (gt.`year`+ 1) = prim.`year` 
-GROUP BY (`year`+ 1)
-;
+GROUP BY (`year`+ 1);
